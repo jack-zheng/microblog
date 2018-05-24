4baa60ff017a4b5a01c2ddd29e3412b2618367c5
jack-zheng <lanmolei0124@163.com>
2018-05-24 22:58:21 +0800

    finish logging config

---
+++ app/errors.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from app import app, db
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    print('404 method been invoked')
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    print('500 method been invoked')
+    db.session.rollback()
+    return render_template('500.html'), 500
+
