66744ec3a10695095c6daf027e7a818106bd2e9b
jack-zheng <lanmolei0124@163.com>
2018-05-27 22:06:24 +0800

    refact show out of user page, import moment lib

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from flask_moment import Moment
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from logging.handlers import RotatingFileHandler
@@ -19,6 +20,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
