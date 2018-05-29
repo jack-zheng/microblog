cfa34b38eb3c5c61c0631e16b81e18b67ad06312
jack-zheng <lanmolei0124@163.com>
2018-05-29 22:00:37 +0800

    finish ajax translation chapter

--- app/__init__.py
+++ app/__init__.py
@@ -56,6 +56,6 @@ if not app.debug:
 @babel.localeselector
 def get_locale():
     #return request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'zh'
+    return 'en'
 
 from app import routes, models, errors
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request, g
 from app import app, db
@@ -8,6 +9,8 @@ from app.models import User, Post
 from werkzeug.urls import url_parse
 from app.email import send_password_reset_email
 from flask_babel import get_locale
+from guess_language import guess_language
+from app.translate import translate
 
 
 @app.before_request
@@ -17,13 +20,26 @@ def before_request():
         db.session.commit()
     g.locale = str(get_locale())
 
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+        request.form['source_language'],
+        request.form['dest_language'])})
+
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN'or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user,
+                language=language)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live !')
